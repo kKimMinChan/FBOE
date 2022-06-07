1469921474ede7ca6d25fd14004fc916c8cfcdca
kKimMinChan <dfmj4520@naver.com>
2022-06-07 21:22:46 +0900

    2022.06.07

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const toggleBtnEl = document.querySelector(".navbar__toogleBtn");
 const menuEl = document.querySelector('.navbar__menu');
 const navbarEl = document.querySelector('.navbar');
@@ -7,14 +6,23 @@ toggleBtnEl.addEventListener('click', () => {
   menuEl.classList.toggle('active')
 })
 
-new Swiper('.experience .swiper-container', {
-  slidesPerView: 1,
-  //spaceBetween: 10,
-  //centeredSlides: true,
+
+//<!-- Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+  rotate: 50,
+  stretch: 0,
+  depth: 100,
+  modifier: 1,
+  slideShadows: true,
+  },
   loop: true,
-  autoplay: {
-      delay: 3000
-  }
-})
-
-
+  autoplay: true,
+  pagination: {
+  el: ".swiper-pagination",
+  },
+});
